5bb6e89b3f86e1a7f0a681e8422f919da7f54534
rayah <rayah@DESKTOP-B58KT0L>
2024-04-05 06:16:42 +0300

    Add project files.

---
+++ ListOfObjects/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace ListOfObjects
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public List<studentClass> StudentList = new List<studentClass>();
+        public MainPage()
+        {
+            InitializeComponent();
+
+           
+
+            StudentList.Add(new studentClass(1,"AA","AAA"));
+            StudentList.Add(new studentClass(2, "BB", "BBB"));
+            StudentList.Add(new studentClass(3, "CC", "CCC"));
+
+
+
+            Stu_List_View.ItemsSource = StudentList;
+
+
+        }
+
+        private async void Stu_List_View_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+            var student = e.Item as studentClass;
+
+            await DisplayAlert("Selected Student", "Student Name:" +
+
+                student.id.ToString(), "Ok"
+                
+                );
+        }
+
+        private void Add_Btn_Clicked(object sender, EventArgs e)
+        {
+
+            int ID;
+            int.TryParse(Stu_id.Text, out ID);
+            StudentList.Add(new studentClass(ID, Stu_name.Text, Stu_department.Text));
+            Stu_List_View.ItemsSource = null;
+            Stu_List_View.ItemsSource = StudentList;
+
+        }
+    }
+
+}
---
+++ ListOfObjects/studentClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfObjects
+{
+    public class studentClass
+    {
+
+        public studentClass(int id, string name, string department)
+        {
+            this.id = id;
+            this.name = name;
+            this.department = department;
+        }
+
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string department { get; set; }
+    }
+}
